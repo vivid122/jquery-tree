d3a4756dff2824ac7ad957b04e941f0a24d49961
vivid122 <983807154@qq.com>
2016-03-05 23:49:01 +0800

    js

--- jquery-tree.js
+++ jquery-tree.js
@@ -1,64 +1,20 @@
+
 $(document).ready(function(){
-	$("li").children().hide();
-	$("li:has(ul)").addClass("subject");
-	$("li ul").addClass("b-sub");
-	$("li").click(function(){
-		if ($(this).is(".subject")) {
-			$(this).children().show();
-			$(this).removeClass("subject").addClass("o-sub");
-		}else if ($(this).is(".o-sub")) {
-			$(this).children().hide();
-			$(this).removeClass("o-sub").addClass("subject");
-		};
-	});
+	$("li:not(:has(ul))").addClass("b-sub");
+	$("li:has(ul)").addClass("o-sub").click(function(){
+		if (this==event.target) {
+		if ($(this).children().is(":visible")) {
+					$(this).children().slideUp('fast', function() {
+						$(this).parent().removeClass()
+							.addClass('subject');
+					});
+				}else{
+					$(this).children().slideDown('fast', function() {
+						$(this).parent().removeClass()
+							.addClass('o-sub');
+					});
+				};
+	};
+  });
 });
 
-
-// $(document).ready(function () {
-//     $("li ul").addClass("b-sub");
-//     $("li:has:(ul)").addClass("subject");
-//     $("li").click(function(){
-//     	if ($(this).children().is(":visible")) {
-//     		$(this).children().hide("fast",function(){
-//     			$(this).parent.class("subject");
-//     		});
-//     	}else{
-//     		$(this).children().slideDown("fast",function(){
-//     			$(this).parent().removeClass("subject").addClass("o-sub");
-//     		});
-//     	};
-//     })
-// 	 });
-
-
-
-// $(document).ready(function(){
-// 	$("li:has(ul)").addClass("subject");
-// 	$("li ul").addClass("b-sub");
-// 	$("li").click(function(){
-// 		$(this).children().toggle();
-// 		if ($(this).is(".subject")) {
-// 			$(this).class("o-sub");
-// 		}else if ($(this).is(".o-sub")) {
-// 			$(this).class("subject");
-// 		};
-// 	});
-// });
-
-
-
-
-
-// $(document).ready(function(){
-// 	$("li:has(ul)").addClass("subject");
-// 	$("li ul").addClass("b-sub");
-// 	$(".subject").click(function(){
-// 		if ($(this).children.is(":visible")){
-// 			$(this).children().hide("fast").display("none");
-// 			$(this).class("subject");
-// 		}else{
-// 			$(this).children().show("fast").display("visible");
-// 			$(this).class("o-sub");
-// 		};
-// 	});
-// });
